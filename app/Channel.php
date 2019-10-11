6722ea917a0b7a9eabfa14395ca6cc3530a4ca97
developer <developer@bloknot.desktop>
2019-10-11 17:41:11 +0300

    channel update authorize

--- app/Channel.php
+++ app/Channel.php
@@ -21,6 +21,11 @@ class Channel extends Model implements HasMedia
         return false;
     }
 
+    public function editable() {
+        if (!auth()->check()) return false;
+        return $this->user_id == auth()->user()->id;
+    }
+
     public function registerMediaConversions(?Media $media = null)
     {
         $this->addMediaConversion('thumb')
