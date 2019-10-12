9bb4f9bfd9e3fbccdb07f285a5b13d90c4c13b4c
Alexander Bezuglov <you@example.com>
2019-10-13 01:02:09 +0300

    formatting subscribers count

--- resources/js/components/subscribe-button.js
+++ resources/js/components/subscribe-button.js
@@ -1,3 +1,5 @@
+import numeral from 'numeral';
+
 Vue.component('subscribe-button', {
     props: {
         channel: {
@@ -20,6 +22,10 @@ Vue.component('subscribe-button', {
 
         owner() {
             if (__auth() && this.channel.user_id === __auth().id) return true;
+        },
+
+        count() {
+            return numeral(this.subscriptions.length).format('0a');
         }
     },
     methods: {
