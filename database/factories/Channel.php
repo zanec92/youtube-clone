892a074fe2afa67c522eb6793c431ea3e097e5bb
Alexander Bezuglov <you@example.com>
2019-10-12 20:03:44 +0300

    database factories, seeders, etc

---
+++ database/factories/Channel.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Faker\Generator as Faker;
+use Laratube\Channel;
+use Laratube\User;
+
+$factory->define(Channel::class, function (Faker $faker) {
+    return [
+        'name' => $faker->sentence(3),
+        'user_id' => function () {
+            return factory(User::class)->create()->id;
+        },
+        'description' => $faker->sentence(30)
+    ];
+});
