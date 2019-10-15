9a6bdd03fd228c7ff83d933b6a2e0141360c3a78
developer <developer@bloknot.desktop>
2019-10-15 14:53:46 +0300

    convert for streaming video

--- app/Jobs/Videos/ConvertForStreaming.php
+++ app/Jobs/Videos/ConvertForStreaming.php
@@ -7,6 +7,8 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use FFMpeg\Format\Video\X264;
+use FFMpeg;
 use Laratube\Video;
 
 class ConvertForStreaming implements ShouldQueue
@@ -32,6 +34,16 @@ class ConvertForStreaming implements ShouldQueue
      */
     public function handle()
     {
-        echo 'converted';
+        $low = (new X264('aac'))->setKiloBitrate(100);
+        $mid = (new X264('aac'))->setKiloBitrate(250);
+        $high = (new X264('aac'))->setKiloBitrate(500);
+
+        FFMpeg::fromDisk('local')
+            ->open($this->video->path)
+            ->exportForHLS()
+            ->addFormat($low)
+            ->addFormat($mid)
+            ->addFormat($high)
+            ->save("public/videos/{$this->video->id}/{$this->video->id}.m3u8");
     }
 }
